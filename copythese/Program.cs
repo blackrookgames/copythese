be64acba56b73a979fde5aca5f46ed3e95b711b3
blackrookgames <blackrookgames@gmail.com>
2024-04-08 20:24:28 -0400

    Add project files.

---
+++ copythese/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace copythese
+{
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.Write("copythese")
+
+                return 0;
+            }
+        }
+    }
+}
---
+++ copythese/Properties/RookInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("copythese")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Black Rook Games")]
+[assembly: AssemblyProduct("copythese")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.1844.21165")]
+[assembly: AssemblyFileVersion("1.0.1844.21165")]
+
+
